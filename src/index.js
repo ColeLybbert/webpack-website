cbad4c4d36a06c19a4bc29079724b6e08df9b313
Cole Lybbert <cole.lybbert65@gmail.com>
2022-11-08 05:07:09 -0800

    api changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-import  handleSubmit  from "./js/formHandler"
-import  checkForName  from "./js/nameChecker"
-
 import './styles/base.scss'
 import './styles/footer.scss'
 import './styles/form.scss'
@@ -12,34 +9,31 @@ const baseURL = "https://api.meaningcloud.com/sentiment-2.1";
 let myForm = document.getElementById('inputForm')
 let button = document.getElementById("submitBtn");
 let formInput = document.getElementById("name");
-
-const apiData = async () => {
-    await cloudData();
-}
-
+let formInputValue = formInput.value;
 
 const cloudData = async () => {
-    try {
-        const formdata = new FormData();
-        formdata.append("key", apiKey);
-        formdata.append("txt", formInput.innerHTML);
-        formdata.append("lang", "en");
-        
-        const requestOptions = {
-          method: 'POST',
-          body: formdata,
-          redirect: 'follow'
-        }
-        
-        const res = await fetch(baseURL, requestOptions)
-        const data = await res.json();
-        console.log(data);
-        return data;
-    } catch (err) {
-        return `Failed ${err}`;
-    }
+  try {
+    // Parameters for the fetch request
+    const formdata = new FormData();
+    formdata.append("key", apiKey);
+    formdata.append("txt", formInputValue);
+    formdata.append("lang", "en");
+    // The fetch request
+    const res = await fetch(baseURL, {
+      method: 'POST',
+      body: FormData(),
+      redirect: 'follow'
+    })
+    // Messing with data
+    const data = await res.json();
+    console.log(data)
+    return data;
+  } catch (err) {
+    return `Failed ${err}`;
+  }
 }
 
+// Telling cloudData to work on button click
 button.addEventListener("click", () => {
-    apiData()
+  cloudData()
   });
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { plugins } = require('@babel/preset-env/lib/plugins-compat-data')
 
 module.exports = {
-    mode: 'production',
+    mode: 'development',
     entry: path.resolve(__dirname, 'src/index.js'),
     output: {
         path: path.resolve(__dirname, 'dist'),
