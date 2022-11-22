0645faaca55aca56eab6c1510645f8eace2da304
Cole Lybbert <cole.lybbert65@gmail.com>
2022-11-21 16:02:45 -0800

    api updates

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import { checkForName } from "./js/nameChecker";
 import { handleSubmit } from "./js/formHandler";
-import { apiFunction } from "./js/api";
 
 import "./styles/base.scss";
 import "./styles/footer.scss";
@@ -13,5 +12,62 @@ export {
   handleSubmit,
 }
 
-apiFunction()
+let myForm = document.getElementById("inputForm");
+let button = document.getElementById("submitBtn");
+let formInput = document.getElementById("name");
+const apiKey = "1d319e9eef10d4332f9a12cd948d7890";
+const baseURL = "https://api.meaningcloud.com/sentiment-2.1";
+
+let apiData;
+let projectData = { apiData };
+
+const sumbitForm = async (e) => {
+  await cloudData();
+
+  postData();
+};
+
+const cloudData = async () => {
+  try {
+    let formInputValue = formInput.value;
+
+    // Parameters for the fetch request
+    const formdata = new FormData();
+    formdata.append("key", apiKey);
+    formdata.append("txt", formInputValue);
+    formdata.append("lang", "en");
+
+    // The fetch request
+    const res = await fetch(baseURL, {
+      method: "POST",
+      body: formdata,
+      redirect: "follow",
+    });
+
+    // Messing with data
+    const data = await res.json();
+    apiData = data;
+    projectData.data = apiData;
+    console.log(apiData)
+    return apiData;
+  } catch (err) {
+    return `Failed ${err}`;
+  }
+};
+
+const postData = async () => {
+  fetch("http://localhost:8000/lang", {
+    method: "POST",
+    headers: { "Content-Type": "application/json"},
+    body: JSON.stringify({projectData}),
+    })
+    .then((response) => response.json())
+};
+
+
+button.addEventListener('click', () => {
+  sumbitForm()
+})
+
+
 
--- src/server/index.js
+++ src/server/index.js
@@ -1,27 +1,28 @@
 var path = require('path')
 const express = require('express')
 
-const app = express()
+const app = express();
 
 app.use(express.static('dist'))
 
 console.log(__dirname)
 
-app.get('/', function (req, res) {
-     res.sendFile('dist/index.html')
-})
+const port = 8000;
+const server = app.listen(port, listening);
 
-// designates what port the app will listen to for incoming requests
-app.listen(8081, function () {
-    console.log('Example app listening on port 8081!')
-})
+function listening() {
+    console.log(`running on localhost:${port}`);
+}
 
 app.get('/lang', (req, res) => {
-    console.log('Data Received');
+    console.log('get request for data came in!Send the tracked object');
     res.send(projectData);
 })
-
+//POST request comes in, this handles it
 app.post('/lang', (req, res) => {
     projectData = req.body;
     console.log(projectData);
 })
+
+
+
