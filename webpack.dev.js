14562727c47c4cd29c02df4703ab201ae5688b79
Cole Lybbert <cole.lybbert65@gmail.com>
2022-11-23 18:26:16 -0800

    script changes in p.json

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,36 +1,52 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const { plugins } = require('@babel/preset-env/lib/plugins-compat-data')
+const webpack = require('webpack')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { plugins } = require("@babel/preset-env/lib/plugins-compat-data");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
-    mode: 'development',
-    entry: path.resolve(__dirname, 'src/index.js'),
-    output: {
-        libraryTarget: 'var',
-        library: 'Client'
-    },
-    module: {
-        rules: [
+  mode: "development",
+  entry: path.resolve(__dirname, "src/index.js"),
+  devtool: 'source-map',
+  stats: 'verbose',
+  output: {
+    libraryTarget: "var",
+    library: "Client",
+  },
+  optimization: {
+    minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
+  },
+  module: {
+    rules: [
         {
-            test:/\.scss$/,
-            use: [
-                'style-loader', 'css-loader', 'sass-loader',
-            ]
+          devServer: {
+            hot: true,
+            publicPath: "/",
+            historyApiFallback: true,
+          },
         },
-        {
-            test: '/\.js$/',
-            exclude: /node_modules/,
-            use: {
-                loader: 'babel-loader',
-            }
-        }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'lang-website',
-            filename: 'index.html',
-            template: 'src/template.html',
-        }),
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: "/.js$/",
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
     ],
-}
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      title: "lang-website",
+      filename: "index.html",
+      template: "src/template.html",
+    }),
+    new MiniCssExtractPlugin({ filename: "[name].css" }),
+  ],
+};
