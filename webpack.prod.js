cec3e81b93e7478c3cd3408456e622ae823780fd
Cole Lybbert <cole.lybbert65@gmail.com>
2022-11-23 16:42:05 -0800

    dev server

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,43 +1,50 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const { plugins } = require('@babel/preset-env/lib/plugins-compat-data')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
+const webpack = require('webpack')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { plugins } = require("@babel/preset-env/lib/plugins-compat-data");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
-    mode: 'production',
-    entry: path.resolve(__dirname, 'src/index.js'),
-    output: {
-        libraryTarget: 'var',
-        library: 'Client'
-    },
-    optimization: {
-        minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
+  mode: "production",
+  entry: path.resolve(__dirname, "src/index.js"),
+  output: {
+    libraryTarget: "var",
+    library: "Client",
+  },
+  optimization: {
+    minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: "/.js$/",
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
         },
-    module: {
-        rules: [
-        {
-            test:/\.scss$/,
-            use: [
-                MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader',
-            ]
+      },
+      {
+        devServer: {
+          hot: true,
+          publicPath: "/",
+          historyApiFallback: true,
         },
-        {
-            test: '/\.js$/',
-            exclude: /node_modules/,
-            use: {
-                loader: 'babel-loader',
-            }
-        }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'lang-website',
-            filename: 'index.html',
-            template: 'src/template.html',
-        }),
-        new MiniCssExtractPlugin({filename: '[name].css'})
+      },
     ],
-}
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      title: "lang-website",
+      filename: "index.html",
+      template: "src/template.html",
+    }),
+    new MiniCssExtractPlugin({ filename: "[name].css" }),
+  ],
+};
