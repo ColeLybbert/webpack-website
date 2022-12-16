c0a5663e3e58c710974027893080101118a01b68
Levy Adams <letitslide87@gmail.com>
2022-12-15 22:15:35 -0500

    dootskie

--- src/index.js
+++ src/index.js
@@ -10,12 +10,12 @@ const submitForm = async () => {
   let formInput = document.getElementById("name").value;
   //in cloud data we return an object with the data we want and assign it here to pass it to next
   //function below it waiting
+  //nothing happens here and also this should be assigned to a variable not just called probably right?s
   await cloudData(formInput);
 
 };
 
-
-
+//start returning values on all functions as a rule of thumb
 export function nameHandle() {
   // check what text was put into the form field
  let formText = document.getElementById("name").value;
@@ -23,8 +23,11 @@ export function nameHandle() {
   return checkForName(formText);
 }
 
+//ternary -> comparison ? true : false
 export function checkForName(inputText) {return inputText === "Kirk"?"welcome Cap!":"Hello Civilian!"}
 
+//you had this outside of the load event, good way to have errors. Run event 
+//listeners on load of document like this
 document.addEventListener('DOMContentLoaded', function () {
   button.addEventListener("click", () => {
     nameHandle();
